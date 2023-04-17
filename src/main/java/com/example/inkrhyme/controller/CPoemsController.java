3f10590d7383b3428df77f4713190f4dbc3eb805
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-04-17 09:16:57 +0200

    update CPoemsController and CService from Elena's branch

---
+++ src/main/java/com/example/inkrhyme/controller/CPoemsController.java
@@ -0,0 +1,42 @@
+package com.example.inkrhyme.controller;
+
+import com.example.inkrhyme.model.CPoems;
+import com.example.inkrhyme.service.CService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/poems")
+@CrossOrigin("*")
+public class CPoemsController {
+    @Autowired
+    CService cService;
+
+    @GetMapping("")
+    private List<CPoems> readPoems(){
+        return cService.readPoems();
+    }
+
+    @GetMapping("/{id}")
+    private Optional<CPoems> readPoemsId(@PathVariable("id") Long id){
+        return cService.readPoemsId(id);
+    }
+
+    @PostMapping("")
+    private void createPoems(@RequestBody CPoems poems){
+        cService.createPoems(poems);
+    }
+
+    @PutMapping("/{id}")
+    private void updatePoems(@RequestBody CPoems poems,@PathVariable("id")Long id){
+        cService.updatePoems(poems,id);
+    }
+
+    @DeleteMapping("/{id}")
+    private void deletePoems(@PathVariable("id")Long id){
+        cService.deletePoems(id);
+    }
+
+}
---
+++ src/main/java/com/example/inkrhyme/service/CService.java
@@ -0,0 +1,40 @@
+package com.example.inkrhyme.service;
+
+import com.example.inkrhyme.model.CPoems;
+import com.example.inkrhyme.repositories.IPoems;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CService {
+    @Autowired
+    private IPoems iPoems;
+
+    public void createPoems(CPoems poems) {
+        iPoems.save(poems);
+    }
+
+    public List<CPoems> readPoems() {
+        List<CPoems> poems = new ArrayList<CPoems>(iPoems.findAll());
+        return poems;
+    }
+
+    public Optional<CPoems> readPoemsId(Long id) {
+        Optional<CPoems> poems = iPoems.findById(id);
+        return poems;
+    }
+
+    public void updatePoems(CPoems poems, Long id) {
+        poems.setId(id);
+        iPoems.save(poems);
+    }
+
+    public void deletePoems(Long id) {
+        iPoems.deleteById(id);
+    }
+
+}
